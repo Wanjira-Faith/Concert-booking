b400a193e0fdb5f6e17be85236c71c24cb41b334
+ <wanjirafaith22@gmail.com>
2023-07-06 12:59:17 +0300

    created function to display list of artist

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function fetchArtists() {
     .then((resp) => resp.json())
     .then((artists) => displayMusicList(artists));
 }
+
+function displayMusicList(artists) {
+    const musicList = document.getElementById("music-list");
+    artists.forEach((artist,index) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = artist.name;
+      listItem.addEventListener("click", () => {
+        displayMusicDetails(artist);
+      });
+      musicList.appendChild(listItem);
+    
+    });
+}    
