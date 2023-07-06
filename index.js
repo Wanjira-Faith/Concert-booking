4526a7528316cdc21ea9adbaaac8e6c0925682b0
+ <wanjirafaith22@gmail.com>
2023-07-06 12:56:48 +0300

    fetched the url and display list of artists

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchArtists();
   });
-  
+  const url = "http://localhost:3000/artists";
+
+function fetchArtists() {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((artists) => displayMusicList(artists));
+}
